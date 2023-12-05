52f5e82213eed67ec59dcd15b7d0053d9c8d68b2
Aydin Elhan <200030355@student.kangan.edu.au>
2023-12-05 15:11:13 +1100

    add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div>
+    <p>Hello, the about page is working now. Yay!</p>
+    <img src="https://media3.giphy.com/media/3o85xsGXVuYh8lM3EQ/giphy.gif?cid=ecf05e47uf8gs5hx22l1bm2a1qdb7dn11ck0pjrhkgfgabki&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="">
+    </div>
