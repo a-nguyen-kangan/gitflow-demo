bc4e30b0971dc6f6a92d9b68dad3784d07170b7c
Aydin Elhan <200030355@student.kangan.edu.au>
2023-12-05 15:17:47 +1100

    about page update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,10 @@
 </script>
 
 <div>
-    <p>Hello, the about page is working now. Yay!</p>
-    <img src="https://media3.giphy.com/media/3o85xsGXVuYh8lM3EQ/giphy.gif?cid=ecf05e47uf8gs5hx22l1bm2a1qdb7dn11ck0pjrhkgfgabki&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="">
-    </div>
+  <p>Hello, the about page is working now. Yay!</p>
+  <img
+    src="https://media3.giphy.com/media/3o85xsGXVuYh8lM3EQ/giphy.gif?cid=ecf05e47uf8gs5hx22l1bm2a1qdb7dn11ck0pjrhkgfgabki&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+    alt=""
+  />
+  <p>Another update because i messed up.</p>
+</div>
